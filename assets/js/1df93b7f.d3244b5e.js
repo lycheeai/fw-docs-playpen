2b5f455c55b4735ed8187c0a09076e2a5e3fc8a2
Jieren Chen <jieren.chen@gmail.com>
2024-10-14 08:03:51 -0400

    new launch

---
+++ assets/js/1df93b7f.d3244b5e.js
@@ -0,0 +1,102 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[583],{6866:(e,i,o)=>{o.r(i),o.d(i,{default:()=>g});o(6540);var r=o(781),n=o(8774),d=o(1227),s=o(4848);const l=d.Ay.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,t=d.Ay.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 100px 160px;
+
+  @media (max-width: 768px) {
+    padding: 80px 40px;
+  }
+
+  gap: 100px;
+`,a=d.Ay.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+
+  padding: 120px 160px;
+  padding-bottom: 0px;
+
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 36px;
+    }
+
+    p {
+      font-size: 16px;
+    }
+    padding: 80px 40px;
+    padding-bottom: 0px;
+  }
+
+  h1 {
+    font-size: 48px;
+  }
+
+  p {
+    font-size: 20px;
+  }
+
+  gap: 16px;
+`,c=d.Ay.div`
+  display: flex;
+  flex-direction: column;
+`,p=d.Ay.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 16px;
+`,x=d.Ay.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+
+  gap: 16px;
+`,h=d.Ay.a`
+  display: flex;
+  flex-direction: column;
+
+  padding: 18px 20px;
+
+  border: 1px solid #e0e0e0;
+  border-radius: 2px;
+
+  transition: border-color 0.3s ease, background-color 0.3s ease;
+  cursor: pointer;
+
+  color: inherit;
+
+  ${e=>{let{disabled:i}=e;return i&&d.AH`
+    opacity: 0.5;
+    cursor: not-allowed;
+  `}}
+
+  &:hover {
+    border-color: #0042FF;
+    background-color: #CED9FC;
+    text-decoration: none;
+    color: inherit;
+  }
+`,f=(0,d.Ay)(n.A)`
+  display: inline-block;
+
+  background-color: rgb(0, 66, 255);
+  color: #fff;
+
+  border-radius: 2px;
+
+  &:hover {
+    background-color: rgb(3, 56, 190);
+  }
+`,u=(0,d.Ay)(n.A)`
+  border-radius: 2px;
+`,b=d.Ay.div`
+  display: flex;
+  gap: 16px;
+`;function g(){return(0,s.jsx)(r.A,{title:"Welcome",description:"Description will go into a meta tag in <head />",children:(0,s.jsxs)(l,{children:[(0,s.jsxs)(a,{children:[(0,s.jsxs)(c,{children:[(0,s.jsx)("h1",{children:"Let your imagination run wild"}),(0,s.jsx)("p",{children:"Build any storefront you can imagine using our flexible headless APIs."})]}),(0,s.jsxs)(b,{children:[(0,s.jsx)(f,{className:"button button--primary",to:"/storefront/getting-started/",children:"Get started"}),(0,s.jsx)(u,{className:"button button--secondary",to:"/storefront/showcase/",children:"Examples"})]})]}),(0,s.jsx)(t,{children:(0,s.jsx)(p,{children:(0,s.jsxs)(x,{children:[(0,s.jsxs)(h,{href:"/storefront/",children:[(0,s.jsx)("h3",{children:"Storefront"}),(0,s.jsx)("p",{children:"Build a custom storefront for your brand."})]}),(0,s.jsxs)(h,{href:"/webhooks/",children:[(0,s.jsx)("h3",{children:"Webhooks"}),(0,s.jsx)("p",{children:"Receive notifications for key events on your shop."})]}),(0,s.jsxs)(h,{disabled:!0,children:[(0,s.jsx)("h3",{children:"Platform Apps"}),(0,s.jsx)("p",{children:"(coming soon)"})]})]})})})]})})}}}]);
